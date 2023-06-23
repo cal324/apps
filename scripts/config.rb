c24a8618f7a9cef96b4c9e9128b4160612493d33
cal324 <1665077+cal324@users.noreply.github.com>
2023-06-23 12:30:31 +0000

    Changed the implementation of the script that creates argocd applicaion settings from envsubst to erb.

--- scripts/config.rb
+++ scripts/config.rb
@@ -1,7 +1,21 @@
 require 'yaml'
+require 'erb'
 
-open('config.yaml'){ |f| YAML.load(f) }.each do |data|
-  env, template, output = %w(env template output).map{|i|data[1][i]}
-  puts "output: #{output}"
-  `. env/#{env}; envsubst < templates/#{template} > ~/apps/#{output}`
+CONFIG_FILE = 'values.yaml'
+TEMPLATE_FILE = 'template.yaml' 
+
+config = YAML.load(open(CONFIG_FILE))
+global = config['global']
+
+config['values'].each do |value|
+  group = value.keys[0]
+  common = value[group]['common']
+  value[group].each do |params|
+    app_name, param = params[0], params[1]
+    if app_name != 'common'
+      yaml = ERB.new(File.read(TEMPLATE_FILE)).result(binding)
+      YAML.dump(YAML.load(yaml), File.open("../#{common['path']}/templates/#{app_name}.yaml", 'w'))
+    end
+  end
 end
+
