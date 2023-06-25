03cbb5f3761deabdb2436969663341562a949464
cal324 <1665077+cal324@users.noreply.github.com>
2023-06-25 07:11:06 +0000

    add fluent spec

--- spec/check_monit.sh
+++ spec/check_monit.sh
@@ -9,3 +9,22 @@ do
   kubectl get apps -A
 done
 
+for i in istio istio-namespace istio-base istio-istiod istio-ingress istio-gateway
+do
+  kubectl wait app -n argocd $i --for=jsonpath='{status.health.status}'=Healthy
+done
+for i in monitoring monitoring-namespace monitoring-crd monitoring-prometheus monitoring-loki monitoring-virtualservice
+do
+  kubectl wait app -n argocd $i --for=jsonpath='{status.health.status}'=Healthy
+done
+
+echo "kubectl get po -A"
+kubectl get po -A
+echo "kubectl get app -A"
+kubectl get app -A
+echo "kubectl get svc -A"
+kubectl get svc -A
+echo "kubectl get ep -A"
+kubectl get ep -A
+echo "kubectl get pv -A"
+kubectl get pv -A
